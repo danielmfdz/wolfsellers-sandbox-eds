dde7e8bbbc22c7f2ad2bb45ef8b5c590c2693395
luis carlos dominguez <luisdominguez@wolfsellers.com>
2025-11-04 17:42:05 -0600

    feat: agrego bloque de cards planes

---
+++ blocks/cardsplans/cardsplans.js
@@ -0,0 +1,19 @@
+export default async function decorate(block) {
+  // const cardsData = [
+  //       {
+  //           price: 100,
+  //           title: "paquete1"
+  //       },
+  //       {
+  //           price: 150,
+  //           title: "paquete2"
+  //       }
+  //   ]
+	// cardsData.forEach(cardData => {
+	// 	let card = `<div>
+
+	// 							</div>`
+	// 	block.insertAdjacentHtml('beforeend', )
+	// })
+	console.log('aqui si jala')
+}
