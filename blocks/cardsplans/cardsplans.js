8592bc9842ff585bb4e523c840e437571ad56d37
luis carlos dominguez <luisdominguez@wolfsellers.com>
2025-11-04 17:48:28 -0600

    fix: linter correcciones

--- blocks/cardsplans/cardsplans.js
+++ blocks/cardsplans/cardsplans.js
@@ -1,19 +1,20 @@
 export default async function decorate(block) {
-  // const cardsData = [
-  //       {
-  //           price: 100,
-  //           title: "paquete1"
-  //       },
-  //       {
-  //           price: 150,
-  //           title: "paquete2"
-  //       }
-  //   ]
+	// const cardsData = [
+	//       {
+	//           price: 100,
+	//           title: "paquete1"
+	//       },
+	//       {
+	//           price: 150,
+	//           title: "paquete2"
+	//       }
+	//   ]
 	// cardsData.forEach(cardData => {
 	// 	let card = `<div>
 
 	// 							</div>`
 	// 	block.insertAdjacentHtml('beforeend', )
 	// })
-	console.log('aqui si jala')
+	const button = document.createElement('button');
+	block.append(button);
 }
