2089961804661de7506c9ddce1ad72a3e0f78d79
luis carlos dominguez <luisdominguez@wolfsellers.com>
2025-11-05 14:42:14 -0600

    fix: implement build configuration

--- blocks/cardsplans/cardsplans.js
+++ blocks/cardsplans/cardsplans.js
@@ -1,7 +1,8 @@
-export default async function decorate(block: HTMLElement) {
+async function decorate(block) {
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('a');
   blockquote.text = 'despues de build';
   blockquote.href = quoteWrapper.textContent;
   quoteWrapper.replaceChildren(blockquote);
 }
+export { decorate as default };
