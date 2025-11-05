3b9cc89cf408a26c443cb42aa965e394eaf5244c
Ernesto Leon <ernestoleon@wolfsellers.com>
2025-11-05 15:12:36 -0600

    fix: configuración para formato crrecto de archivos js.

--- blocks/cardsplans/cardsplans.js
+++ blocks/cardsplans/cardsplans.js
@@ -1,8 +1,8 @@
-async function decorate(block) {
+// blocks/cardsplans/cardsplans.ts
+export default async function decorate(block) {
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('a');
   blockquote.text = 'despues de build';
   blockquote.href = quoteWrapper.textContent;
   quoteWrapper.replaceChildren(blockquote);
 }
-export { decorate as default };
