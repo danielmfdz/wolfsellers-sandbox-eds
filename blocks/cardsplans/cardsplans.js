867683b605c5137517e704d3674f60b595520d3d
Ernesto Leon <ernestoleon@wolfsellers.com>
2025-11-05 17:49:33 -0600

    fix: se arregla name en definition.

--- blocks/cardsplans/cardsplans.js
+++ blocks/cardsplans/cardsplans.js
@@ -1,5 +1,6 @@
 // blocks/cardsplans/cardsplans.ts
 export default async function decorate(block) {
+  console.log(block);
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('a');
   blockquote.text = 'despues de build';
