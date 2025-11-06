a9b841d782dbb9d5ef802b9dd74ee27d4818ac12
luis carlos dominguez <luisdominguez@wolfsellers.com>
2025-11-06 14:24:06 -0600

    feat: add styles and behaviour

--- blocks/cardsplans/cardsplans.ts
+++ blocks/cardsplans/cardsplans.ts
@@ -1,7 +1,38 @@
+type PackageData = {
+  key: string;
+  expiration: string;
+  name: string;
+  price: number;
+  megabytesIncluded: number;
+}
+
 export default async function decorate(block: HTMLElement) {
-  const [quoteWrapper] = block.children;
-  const blockquote = document.createElement('a');
-  blockquote.text = 'despues de build';
-  blockquote.href = quoteWrapper.textContent;
-  quoteWrapper.replaceChildren(blockquote);
+  const res = await fetch('https://publish-p153442-e1604645.adobeaemcloud.com/graphql/execute.json/Sandbox-Models/get-all-cards-plans');
+  const { data } = await res.json();
+  const { cardsPlansModelList: { items: [{ cardData: [{ packages }] }] } } = data;
+  let elementString: string = '<div class="card-container">';
+  packages.forEach((plan:PackageData) => {
+    elementString += `<div class="card-plan">
+        <div class="card-plan-title">
+          <p>${plan.name}s</p>
+        </div>
+        <div class="card-plan-body">
+          <div>
+            <p class="card-plan-body__cantidad">${plan.megabytesIncluded < 0 ? 'Ilimitados' : plan.megabytesIncluded}</p>
+            <p>Datos</p>
+          </div>
+          <div class="just-i-end">
+            <p class="card-plan-body__price">${plan.price}</p>
+            <button class="card-plan-body-btn__iwant">Lo quiero</button>
+          </div>
+        </div>
+        <div class="card-plan-footer">
+          <div class="">
+            <p>${plan.expiration}</p>
+          </div>      
+        </div>
+      </div>`;
+  });
+  elementString += '</div>';
+  block.insertAdjacentHTML('beforeend', elementString);
 }
